4da4b63e198a05fe93ad4ac9ef64ff6824d13a1c
hagar2003 <hagaradeahmed386@gmail.com>
2024-08-19 20:11:27 +0300

    best seller list view item part 1

--- lib/features/splash/presentation/manager/views/widgets/home_view_body.dart
+++ lib/features/splash/presentation/manager/views/widgets/home_view_body.dart
@@ -1,4 +1,4 @@
-
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/splash/presentation/manager/views/widgets/books_list_view.dart';
 import 'package:bookly_app/features/splash/presentation/manager/views/widgets/custom_app_bar.dart';
@@ -12,16 +12,44 @@ class HomeBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
-        children: [CustomAppBar(), 
-        BooksListView(),
-        Container(width: double.infinity,
-          child: Text('   Best Seller',
-          textAlign: TextAlign.start,style:Styles.titleMed
-          ,),
-        )],
+        children: [
+          CustomAppBar(),
+          BooksListView(),
+          Container(
+            width: double.infinity,
+            child: Text(
+              '   Best Seller',
+              textAlign: TextAlign.start,
+              style: Styles.titleMed,
+            ),
+          ),
+          BestSellerListViewItem()
+        ],
       ),
     );
   }
 }
 
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(height: 120,
+      child: Row(
+        children: [
+        AspectRatio(
+        aspectRatio: 2.8 / 4,
+        child: Container(
+          decoration: BoxDecoration(color: Colors.white,
+              borderRadius: BorderRadius.circular(8),
+              image: DecorationImage(
+                  fit: BoxFit.fill,
+                   image: AssetImage(AssetData.testt))),
+        ),
+      )
+        ],
+      ),
+    );
+  }
+}
