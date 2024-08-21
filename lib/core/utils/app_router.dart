794825ddbf3fcf5e185bc5d003ea0c2d39a98980
hagar2003 <hagaradeahmed386@gmail.com>
2024-08-22 01:13:03 +0300

    create search view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,23 +1,29 @@
 import 'package:bookly_app/features/home/presentation/view_models/views/book_details_view.dart';
 import 'package:bookly_app/features/home/presentation/view_models/views/home_view.dart';
 import 'package:bookly_app/features/home/presentation/view_models/views/splash_view.dart';
+import 'package:bookly_app/features/search/presentation/views/search_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/HomeView';
   static const kBookDetailsView = '/bookDetailsView';
+    static const kSearchView = '/searchView';
   static final router = GoRouter(routes: [
     GoRoute(
       path: ('/'),
       builder: (context, state) => const SplashView(),
     ),
     GoRoute(
-      path: ('/homeView'),
+      path: kHomeView,
       builder: (context, state) => const HomeView(),
     ),
     GoRoute(
       path: ('/bookDetailsView'),
       builder: (context, state) => const BookDetailsView(),
-    )
+    ),
+      GoRoute(
+      path: kSearchView,
+      builder: (context, state) => const SearchView(),
+    ),
   ]);
 }
--- lib/features/home/presentation/view_models/views/widgets/book_details_body.dart
+++ lib/features/home/presentation/view_models/views/widgets/book_details_body.dart
@@ -5,6 +5,7 @@ import 'package:bookly_app/features/home/presentation/view_models/views/widgets/
 import 'package:bookly_app/features/home/presentation/view_models/views/widgets/books_action.dart';
 import 'package:bookly_app/features/home/presentation/view_models/views/widgets/books_list_view.dart';
 import 'package:bookly_app/features/home/presentation/view_models/views/widgets/custom_list_view_item.dart';
+import 'package:bookly_app/features/home/presentation/view_models/views/widgets/similar_books_list_view.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsBody extends StatelessWidget {
@@ -15,74 +16,62 @@ class BookDetailsBody extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: Column(
-          children: [
-          SafeArea(
-              child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: AppBarDetails(),
-          )),
-          Padding(
-            padding: EdgeInsets.symmetric(
-                horizontal: MediaQuery.of(context).size.width * .25),
-            child: ListViewItem(),
-          ),
-          SizedBox(
-            height: 36,
-          ),
-          Text(
-            'The Jungle Book',
-            style: Styles.textStyle30,
-          ),
-          SizedBox(
-            height: 6,
-          ),
-          Text(
-            'Rudyard Kipling',
-            style: Styles.textStyle18
-                .copyWith(color: Color(0xff707070), fontStyle: FontStyle.italic),
-          ),
-          SizedBox(
-            height: 14,
-          ),
-          Row(
+        child: CustomScrollView(
+          slivers: [  
+            SliverFillRemaining(
+hasScrollBody: false,
+        
+          child: Column(
             children: [
-              SizedBox(
-                width: 160,
-              ),
-              BookRating(),
-              
-            ],
-          ), 
-          SizedBox(height: 37,), Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: BooksAction(),
-          ),SizedBox(height: 50,),
-          Container(width: double.infinity,
-            child: Text('You can also like',style: Styles.textStyle16,)),
-            SizedBox(height: 16,),
-          SimilarListView()  
-        ]),
-      ),
-    );
+            SafeArea(
+                child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: AppBarDetails(),
+            )),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.of(context).size.width * .25),
+              child: ListViewItem(),
+            ),
+            SizedBox(
+              height: 36,
+            ),
+            Text(
+              'The Jungle Book',
+              style: Styles.textStyle30,
+            ),
+            SizedBox(
+              height: 6,
+            ),
+            Text(
+              'Rudyard Kipling',
+              style: Styles.textStyle18
+                  .copyWith(color: Color(0xff707070), fontStyle: FontStyle.italic),
+            ),
+            SizedBox(
+              height: 14,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 160,
+                ),
+                BookRating(),
+                
+              ],
+            ), 
+            SizedBox(height: 37,), Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: BooksAction(),
+            ),SizedBox(height: 50,),
+            Container(width: double.infinity,
+              child: Text('  You can also like',style: Styles.textStyle16,)),
+              SizedBox(height: 16,),
+            SimilarListView()  
+          ]),
+        ),
+      ]),
+    ));
   }
 }
 
-class SimilarListView extends StatelessWidget {
-  const SimilarListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return  SizedBox( height: MediaQuery.of(context).size.height * .15,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: 10,
-        itemBuilder: (context, index) {
-        return Padding(
-          padding:const EdgeInsets.symmetric(horizontal: 8.0),
-          child: ListViewItem(),
-        );
-      }),
-    );
-  }
-}
