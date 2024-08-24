27552f7d3924877ed18d71bc911b514c912c5f0d
hagar2003 <hagaradeahmed386@gmail.com>
2024-08-24 23:03:51 +0300

    featured books cubit

--- lib/features/home/presentation/manager/featured_books/featurd_books_cubit.dart
+++ lib/features/home/presentation/manager/featured_books/featurd_books_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/models/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'featurd_books_state.dart';
 
 class FeaturdBooksCubit extends Cubit<FeaturdBooksState> {
-  FeaturdBooksCubit() : super(FeaturdBooksInitial());
+  FeaturdBooksCubit(this.homeRepo) : super(FeaturdBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturdBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure) {
+      emit(FeaturdBooksFailure(failure.errorMessage));
+    }, (books) {
+      emit(FeaturdBooksSuccess(books));
+    });
+  }
 }
