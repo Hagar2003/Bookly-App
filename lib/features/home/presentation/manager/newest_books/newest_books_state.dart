dd690d494981cc19c250c89f39e5e61073468b48
hagar2003 <hagaradeahmed386@gmail.com>
2024-08-24 23:08:22 +0300

    newest books states

---
+++ lib/features/home/presentation/manager/newest_books/newest_books_state.dart
@@ -0,0 +1,21 @@
+part of 'newest_books_cubit.dart';
+
+@immutable
+sealed class NewestBooksState {}
+
+final class NewestBooksInitial extends NewestBooksState {}
+
+final class NewestBooksLoading extends NewestBooksState {}
+
+final class NewestBooksFailure extends NewestBooksState {
+  final String erMessage;
+
+  NewestBooksFailure(this.erMessage);
+}
+
+final class NewestBooksSuccess extends NewestBooksState {
+  final List<BookModel> books;
+
+  NewestBooksSuccess(this.books);
+
+}
