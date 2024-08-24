cdd28e649ffc79a568a56c9d12328ee004dd45b4
hagar2003 <hagaradeahmed386@gmail.com>
2024-08-24 22:37:26 +0300

    create server failure

--- lib/features/home/data/repos/home_repo_implementation.dart
+++ lib/features/home/data/repos/home_repo_implementation.dart
@@ -1,8 +1,11 @@
+import 'dart:math';
+
 import 'package:bookly_app/core/errors/failures.dart';
 import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:bookly_app/features/home/data/models/book_model.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImplementation implements HomeRepo {
   final ApiService apiService;
@@ -19,8 +22,9 @@ class HomeRepoImplementation implements HomeRepo {
       }
       return right(books);
     } catch (e) {
-      return left(ServerFailure());
+      if (e is DioException) return left(ServerFailure.fromDioException(e));
     }
+    return left(ServerFailure(e.toString()));
   }
 
   @override
